8b6dbc0f03b40a8cfbe4ffdcd7e44e623a9088f9
paez98 <adrianpz1198@gmail.com>
2025-06-19 00:53:36 -0400

    Second commit

--- ejercicio_api.py
+++ ejercicio_api.py
@@ -1,41 +1,56 @@
 import requests
 
 
-def obtener_tareas_completadas(rangos_id):
+def obtener_tareas_completadas(all: bool = True) -> list:
     tareas_completadas = []
-    for tarea_id in range(rangos_id[0], rangos_id[1] + 1):
-        url = f"https://jsonplaceholder.typicode.com/todos/{tarea_id}"
 
+    if not all:
+
+        id_inicial = int(input("Ingrese el ID de la primera tarea: "))
+        id_final = int(input("Ingrese el ID de la ultima tarea: "))
+        rangos_id = (id_inicial, id_final)
+        for tarea_id in range(rangos_id[0], rangos_id[1] + 1):
+            url = f"https://jsonplaceholder.typicode.com/todos/{tarea_id}"
+
+            try:
+                response = requests.get(url)
+                response.raise_for_status()
+
+                tarea = response.json()
+
+                if tarea.get('completed'):
+                    titulo = tarea.get('title')
+                    id_obtenido = tarea.get('id')
+                    if titulo:
+                        tareas_completadas.append(
+                            {'id': id_obtenido, 'title': titulo})
+                    else:
+                        print(
+                            f"La tarea {tarea_id} esta completada pero no tiene titulo.")
+
+            except requests.exceptions.RequestException as e:
+                print(f"Error al procesar la tarea {tarea_id}: {e}")
+            except Exception as e:
+                print(
+                    f"Ocurrió un error inesperado al procesar la tarea {tarea_id}: {e}")
+    else:
+        url = "https://jsonplaceholder.typicode.com/todos"
         try:
             response = requests.get(url)
             response.raise_for_status()
 
-            tarea = response.json()
-
-            if tarea.get('completed'):
-                titulo = tarea.get('title')
-                id_obtenido = tarea.get('id')
-                if titulo:
-                    tareas_completadas.append(
-                        {'id': id_obtenido, 'title': titulo})
-                else:
-                    print(
-                        f"La tarea {tarea_id} esta completada pero no tiene titulo.")
+            tareas = response.json()
+            for tarea in tareas:
+                tareas_completadas.append(tarea)
 
         except requests.exceptions.RequestException as e:
-            print(f"Error al procesar la tarea {tarea_id}: {e}")
+            print(f"Error al obtener las tareas: {e}")
         except Exception as e:
-            print(
-                f"Ocurrió un error inesperado al procesar la tarea {tarea_id}: {e}")
-
+            print(f"Ocurrió un error inesperado: {e}")
     return tareas_completadas
 
 
-tarea_inicial = int(input("Ingrese el ID de la primera tarea: "))
-tarea_final = int(input("Ingrese el ID de la ultima tarea: "))
-rango_de_tareas = (tarea_inicial, tarea_final)
-
-titulos_tareas = obtener_tareas_completadas(rango_de_tareas)
+titulos_tareas = obtener_tareas_completadas(all=False)
 
 if titulos_tareas:
     print(f"Tareas completadas:{len(titulos_tareas)}")
